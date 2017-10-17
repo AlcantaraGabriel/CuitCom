b6de18713fb3f519434c16104fa3ab9beca9e07d
Gabriel Alcântara <gabrielalcantara39@gmail.com>
2017-10-17 12:08:00 -0200

    Add files via upload

--- testSchemDraw.py
+++ testSchemDraw.py
@@ -1,16 +1,13 @@
-# Desenho de um circuito usando a biblioteca SchemDraw:https://pypi.python.org/pypi/SchemDraw
-# Autor: Gabriel Alc√¢ntara Silva, 01/09/2017.
-#OBS. Esse √© um pequeno teste da biblioteca, ela tem mais recursos.
-
+# -*- coding: cp1252 -*-
 import SchemDraw as schem
 import SchemDraw.elements as e
 import SchemDraw.logic as l
 
 d = schem.Drawing()
-#   * = N√≥
-# --- = linha horizontal com comprimento padr√£o(3.0)
+#   * = NÛ
+# --- = linha horizontal com comprimento padr„o(3.0)
 #  -- = linha horizontal com comprimento modificado
-#  ||| = linha vertical com comprimento padr√£o(3.0)
+#  ||| = linha vertical com comprimento padr„o(3.0)
 #   || = linha vertical com comprimento modificado
 
 
@@ -54,8 +51,8 @@ d.add(e.LINE,l = 0.8,d = 'right',xy = [2.8 , -4.85])#--
 d.add(e.DOT,d = 'down',xy = [2.8 , -4.85])#*
 
 #*--
-d.add(e.LINE,l = 0.8,d = 'right',xy = [2.8 , -6.55])#--
-d.add(e.DOT,d = 'down',xy = [2.8 , -6.55])#*
+d.add(e.LINE,l = 1.8,d = 'right',xy = [2 , -6.55])#--
+d.add(e.DOT,d = 'down',xy = [2 , -6.55])#*
 
 
 
@@ -63,4 +60,16 @@ d.add(e.DOT,d = 'down',xy = [2.8 , -6.55])#*
 d.add(e.DOT,d = 'right',xy = [0.0 , -3.0])#*
 d.add(e.LINE,l = 7.0,d = 'right',xy = [0.0,-3.0],rgtlabel ='$S1$')#---S1
 
+
+
+#G1 = d.add(l.NOT,d = 'down' ,anchor='in1')
+
+#no1 = d.add(e.DOT,xy= [A.here[0],A.here[1]])
+
+#B = d.add(e.DOT,toplabel ='$B$',xy = [A.here[0]+3,A.here[1]])
+
+
+#S = d.add(e.LINE, rgtlabel='$S$')
+#A = d.add(e.DOT, xy=S.here)
+#d.add(e.LINE, d='up', l=d.unit*1, ilabel='$A$')
 d.draw()
